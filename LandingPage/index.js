92dfa3d7b200cb6cd2cd95f02408f685d1f5f48a
TOOBAD3060 <olalerebabatunde2000@gmail.com>
2023-02-15 15:23:37 +0100

    fullImgFunctionality

--- LandingPage/index.js
+++ LandingPage/index.js
@@ -1,6 +1,12 @@
 //  Getting elements
 
 const slicedText = document.getElementById("sliced-text")
+const descImgContainer = document.querySelector(".desc-images ")   //conatiner show all desc img
+const descImg = document.querySelectorAll(".desc-images img")
+const fullDescImgContainer = document.querySelector(".desc-images-full-view")  // container for full img (the clicked one)
+const fullDescImg = document.querySelector(".desc-images-full-view img")
+const imgCloseBtn = document.querySelector(".desc-images-full-view #close-btn")
+
 //  sliced text
 
 const theText = "Innovating with Technology";
@@ -13,3 +19,33 @@ function slicer() {
     setTimeout(slicer,100)
 }
 
+// show full pics 
+
+
+//   =    "
+
+let currentFullImg = "";
+
+descImg.forEach((item,index)=>{
+     item.addEventListener("click",()=>{
+
+        // get the clicked image
+        currentFullImg = item.getAttribute("src");
+        //  set the full img to display
+        fullDescImg.setAttribute("src",currentFullImg);   
+        
+         // show full image
+     fullDescImgContainer.classList.add("active")
+
+    //  hide all images
+    descImgContainer.classList.add("inactive") 
+        } )
+})
+
+imgCloseBtn.addEventListener("click",()=>{
+    // hide full image
+    fullDescImgContainer.classList.remove("active")
+
+    //  show all images
+    descImgContainer.classList.remove("inactive")
+})
